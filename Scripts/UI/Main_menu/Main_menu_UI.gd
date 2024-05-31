95a6c1d90ae805979b9b3e56fa7c60f16679403f
sergio monfort <sergio.monfort1@gmail.com>
2024-05-31 20:29:50 +0200

    menu rework

--- Scripts/UI/Main_menu/Main_menu_UI.gd
+++ Scripts/UI/Main_menu/Main_menu_UI.gd
@@ -1,12 +1,17 @@
 extends Control
 
 @onready var animationPlayer : AnimationPlayer = $AnimationPlayer
+@onready var startContainer : CenterContainer = $StartContainer
+@onready var mainMenuContainer : VBoxContainer = $HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	mainMenuContainer.visible = false
+	startContainer.visible = true
+	animationPlayer.play("Breathing_Anim")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("Jump"):
+		animationPlayer.play("Startup_Anim")
