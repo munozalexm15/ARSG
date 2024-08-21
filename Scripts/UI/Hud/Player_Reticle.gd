19a01b44d67a331d1136ca63117e16713bf0a7a7
munozalexm15 <munozalexm15@gmail.com>
2024-08-21 18:53:37 +0200

    forgot to parse to int the max value for teams progress bar

--- Scripts/UI/Hud/Player_Reticle.gd
+++ Scripts/UI/Hud/Player_Reticle.gd
@@ -63,8 +63,8 @@ func _ready():
 	NPCRoleLabel.visible = false
 	pointsContainer.visible = false
 	timerContainer.visible = false
-	Team1ProgressBar.max_value = Network.game.matchGoal
-	Team2ProgressBar.max_value = Network.game.matchGoal
+	Team1ProgressBar.max_value = int(Network.game.matchGoal)
+	Team2ProgressBar.max_value = int(Network.game.matchGoal)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
