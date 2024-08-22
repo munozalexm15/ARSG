5bf4b514d72cea0ebbb83c75cb5eb25a25e3f8fe
munozalexm15 <munozalexm15@gmail.com>
2024-08-22 17:25:26 +0200

    ??

--- Scripts/Player/Main_character.gd
+++ Scripts/Player/Main_character.gd
@@ -310,6 +310,8 @@ func updateHealth():
 
 @rpc("any_peer", "call_local")
 func die_respawn(player_id, instigator_id):
+	if Network.game == null:
+		return
 	#actualizar los diccionarios de todos los jugadores con los stats
 	var dead_guy = Network.game.players["player"+ str(player_id)]
 	var killer = Network.game.players["player"+ str(instigator_id)]
