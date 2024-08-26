78fc70ab9bbbd5022eb14b53f699381f1c66aa48
munozalexm15 <munozalexm15@gmail.com>
2024-08-26 20:54:47 +0200

    a

--- Scripts/Multiplayer/Network.gd
+++ Scripts/Multiplayer/Network.gd
@@ -179,4 +179,3 @@ func load_main_menu():
 @rpc("reliable", "any_peer", "call_local")
 func kicked():
 	peer.disconnect_peer(multiplayer.get_unique_id())
-	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
