9a08c7960459a303c3f7894df0e25a382e4d9fd2
munozalexm15 <munozalexm15@gmail.com>
2024-08-26 12:00:27 +0200

    adding call deferred to prevent crashes

--- Scripts/Multiplayer/Network.gd
+++ Scripts/Multiplayer/Network.gd
@@ -158,6 +158,5 @@ func show_all_players():
 		if player.arms.weaponHolder.get_child_count() > 0:
 			player.visible = true
 
-@rpc("reliable", "any_peer", "call_local")
 func endGame():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
