8ab802714ef4a90a0b82551a21c8a3995d7f578b
munozalexm15 <munozalexm15@gmail.com>
2024-08-28 18:06:23 +0200

    a

--- Scripts/UI/Pause_menu/pause_menu.gd
+++ Scripts/UI/Pause_menu/pause_menu.gd
@@ -79,13 +79,9 @@ func _on_exit_match_button_pressed():
 
 	#Network.player_left.rpc(multiplayer.get_unique_id())
 	Steam.leaveLobby(Network.lobby_id)
-	for x in Steam.getNumLobbyMembers(Network.lobby_id):
-		var member_steam_id = Steam.getLobbyMemberByIndex(Network.lobby_id, x)
-		var member_peer_id = Network.peer.get_peer_id_from_steam64(member_steam_id)
-		if multiplayer.get_unique_id() != member_peer_id:
-			print("closing session")
-			Steam.closeP2PSessionWithUser(member_steam_id)
-	
+	var lobbyId = Network.lobby_id 
 	Network.lobby_id = 0
-	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
-	
+	for x in Steam.getNumLobbyMembers(lobbyId):
+		var member_steam_id = Steam.getLobbyMemberByIndex(lobbyId, x)
+		if Steam.getSteamID() != member_steam_id:
+			Steam.closeP2PSessionWithUser(member_steam_id)
