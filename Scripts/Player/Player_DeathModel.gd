049bb0255c6533f57a3725abe1203dfada6d1946
Alejandro Muñoz <munozalexm15@gmail.com>
2024-09-24 20:49:56 +0200

    trying to fix animation death stuck

--- Scripts/Player/Player_DeathModel.gd
+++ Scripts/Player/Player_DeathModel.gd
@@ -6,13 +6,16 @@ extends Node3D
 @onready var parent : Node3D = $ModelParent
 
 @onready var playerMesh : MeshInstance3D = $ModelParent/Armature/Skeleton3D/PoliceOfficer
-
+var anims = animationPlayer.get_animation_list()
+var selectedPos = randi() % anims.size()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scale = Vector3(0.5, 0.5, 0.5)
-	var anims = animationPlayer.get_animation_list()
-	animationPlayer.play(anims[randi() % anims.size()])
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	animationPlayer.play(anims[selectedPos])
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	await get_tree().create_timer(5).timeout
+	queue_free()
