aef92d71fc1c2d94005e02812aa8561a1c112c33
Alejandro Muñoz <munozalexm15@gmail.com>
2024-10-06 15:11:36 +0200

    fixed flying dead bodies

--- Scripts/Player/Player_DeathModel.gd
+++ Scripts/Player/Player_DeathModel.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
 
-@onready var animationPlayer : AnimationPlayer = $ModelParent/AnimationPlayer
+@onready var animationPlayer : AnimationPlayer = $RigidBody3D/ModelParent/AnimationPlayer
 
-@onready var parent : Node3D = $ModelParent
+@onready var parent : Node3D = $RigidBody3D/ModelParent
 
-@onready var playerMesh : MeshInstance3D = $ModelParent/Armature/Skeleton3D/PoliceOfficer
+@onready var playerMesh : MeshInstance3D = $RigidBody3D/ModelParent/Armature/Skeleton3D/PoliceOfficer
+@onready var collShape : CollisionShape3D = $RigidBody3D/CollisionShape3D
 var anims = null
 var selectedPos = null
 
@@ -16,6 +17,7 @@ func _ready():
 	anims = animationPlayer.get_animation_list()
 	selectedPos = randi() % anims.size()
 	animationPlayer.play(anims[selectedPos])
+	parent.scale = Vector3(0.5, 0.5, 0.5)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
