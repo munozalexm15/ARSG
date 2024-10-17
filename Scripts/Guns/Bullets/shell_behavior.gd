3ca760573f085e9838dc09d40e03407a65870e9f
Alejandro Muñoz <munozalexm15@gmail.com>
2024-10-17 16:32:49 +0200

    refactor things from yesterday that I did at 3 AM and I was so fed up sorry me from the past

--- Scripts/Guns/Bullets/shell_behavior.gd
+++ Scripts/Guns/Bullets/shell_behavior.gd
@@ -15,21 +15,17 @@ var instigator : Player
 var damage : float
 var distanceTraveled: float = 0
 var decal_instance : Decal
-var ready_to_move = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().process_frame
-	ready_to_move = true
 	await get_tree().create_timer(5).timeout
 	queue_free()
 
 func _physics_process(delta: float) -> void:
 	distanceTraveled += 0.0001
-	if ready_to_move:
-		position += transform.basis * Vector3(0,0,-SPEED)
-		if ray.is_colliding():
-			_on_area_3d_body_entered(ray.get_collider())
+	position += transform.basis * Vector3(0,0,-SPEED)
+	if ray.is_colliding():
+		_on_area_3d_body_entered(ray.get_collider())
 
 func _on_area_3d_body_entered(body : Node3D):
 	print(body.name)
@@ -44,20 +40,19 @@ func _on_area_3d_body_entered(body : Node3D):
 		
 		queue_free()
 	
-	if body is Player:# and body != instigator:
-		var playerHit : Player = body.get_parent()
+	if body is Player and body != instigator:
 		var audioSteam : AudioStream = load("res://GameResources/Sounds/Misc/hitmarker_sound.wav")
 		SFXHandler.play_sfx(audioSteam, instigator, "Effects")
-		playerHit.health -= damage - distanceTraveled
+		body.health -= damage - distanceTraveled
 		playerDamaged.emit()
 		if instigator.hud.animationPlayer.is_playing():
 			instigator.hud.animationPlayer.play("RESET")
 		instigator.hud.animationPlayer.play("hitmarker")
 		
-		playerHit.assign_enemy_to_player_hit.rpc_id(playerHit.name.to_int(), instigator.name.to_int(), playerHit.name.to_int())
-		if playerHit.health <= 0 and playerHit.visible == true:
+		body.assign_enemy_to_player_hit.rpc_id(body.name.to_int(), instigator.name.to_int(), body.name.to_int())
+		if body.health <= 0 and body.visible == true:
 			instigator.hud.killPointsAnimPlayer.play("killNotification")
-			playerHit.die_respawn.rpc(playerHit.name.to_int(), instigator.name.to_int())
+			body.die_respawn.rpc(body.name.to_int(), instigator.name.to_int())
 	
 		queue_free()
 	
